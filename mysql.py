ec200f2ac73c0bf12c235829b98f702e29482e4c
premdub <111820562+premdub@users.noreply.github.com>
2022-10-09 16:50:12 -0400

    update

--- mysql.py
+++ mysql.py
@@ -1,21 +1,33 @@
 #!sudo apt-get install python3-dev default-libmysqlclient-devsudo apt-get install python3-dev default-libmysqlclient-dev
 #pip install pymysql sqlalchemy
+# pip install -u python-dotenv or
+#pip install python-dotenv
 from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
-MYSQL_HOSTNAME = '35.193.75.235'
-MYSQL_USER = 'pdubey'
-MYSQL_PASSWORD = 'prem4321'
-MYSQL_DATABASE ='real_df'
-connect_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
-connect_string
-db = create_engine(connect_string)
-db
+import MySQLdb
+from dotenv import load_dotenv
 
-#db = pd.read_sql(query,con=db)
-#db
+load_dotenv()
+print(os.getenv("PATH"))
+
+MYSQL_HOSTNAME = os.getenv('MYSQL_HOSTNAME')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DATABASE =('brain_size_df')
+
+connect_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_DATABASE}/{MYSQL_DATABASE}'
+connect_string
+
+db = create_engine(connect_string)
+
+
+
+
 brain_size_df = pd.read_csv('https://raw.githubusercontent.com/premdub/descriptives_scipy/main/data/brain_size.csv')
 brain_size_df
+
+
 brain_size_df.to_sql('brain_size_table', con=db, if_exists="replace")
 
 
